79a4a88ca0b5bd7c8299313f43805acdcc1751dd
parkysood <parkysood@gmail.com>
2025-05-18 13:24:49 -0700

    Added tailwind setup

--- nebula/src/App.tsx
+++ nebula/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Sidebar from './components/Sidebar';
+// import './App.css';
 import Flashcard from './pages/Flashcard';
 
 function App() {
