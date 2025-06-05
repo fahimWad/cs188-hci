9e024d87283dfa80beeaf9c57d25945fbe2a48f6
parkysood <parkysood@gmail.com>
2025-06-04 20:23:32 -0700

    fixed overflow... still have other bugs

--- nebula/src/components/flashcard_components/DeleteButton.tsx
+++ nebula/src/components/flashcard_components/DeleteButton.tsx
@@ -1,38 +1,41 @@
-import React from 'react';
+import React from "react";
 import { IoMdTrash } from "react-icons/io";
 
 type DeleteButtonProps = {
-    onClick: () => void;
-    color: string;
-    disabled?: boolean;
-    children?: React.ReactNode;
-    isVisible?: boolean;
+	onClick: () => void;
+	color: string;
+	disabled?: boolean;
+	children?: React.ReactNode;
+	isVisible?: boolean;
+	isActive?: boolean;
 };
 
 const colorClassMap: { [key: string]: string } = {
-    red: 'text-red-500 -translate-x-1/4',
-    lavender: 'border-primary-3 bg-primary-3 text-white text-xl',          // no hovering animation, just override solid color for sidebar
+	red: "text-red-500 -translate-x-1/4",
+	lavender: "border-primary-3 bg-primary-3 text-white text-xl", // no hovering animation, just override solid color for sidebar
 };
 
 const DeleteButton: React.FC<DeleteButtonProps> = ({
-    onClick,
-    color,
-    isVisible,
+	onClick,
+	color,
+	isVisible,
+	isActive,
 }) => (
-    <button
-        className={`
+	<button
+		className={`
             p-0.5 rounded-lg
-            cursor-pointer
-            opacity-100
-            ${colorClassMap[color] || ''}
+            ${isActive ? "opacity-100 cursor-pointer" : "opacity-50"}
+            ${colorClassMap[color] || ""}
         `}
-        type="button"
-        onClick={onClick}
-        aria-label="Delete"
-        title="Delete"
-    >
-        <IoMdTrash />
-    </button>
+		type="button"
+		onClick={onClick}
+		aria-label="Delete"
+		title="Delete"
+		disabled={!isActive}
+		aria-disabled={!isActive}
+	>
+		<IoMdTrash />
+	</button>
 );
 
 export default DeleteButton;
